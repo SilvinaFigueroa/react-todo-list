e83e1ddd72bc058033ea5a71654c61659089438d
Silvina Figueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2024-07-13 19:58:00 -0700

    edit task implementation and conditional rendering on TodoItemList component

--- src/components/TodoItemList.jsx
+++ src/components/TodoItemList.jsx
@@ -1,37 +1,63 @@
+import { useState } from "react"
+
+const TodoItemList = ({ task, dispatch }) => {
+  const [taskUpdate, setTaskUpdate] = useState('')
+  const [editButton, setEditButton] = useState(false)
 
 
-const TodoItemList = ({task, dispatch}) => {
+  function editToggle() {
+    setEditButton(!editButton)
+    console.log(`editButton ${editButton}`)
+  }
+
+  function editTask(e) {
+    e.preventDefault()
+    dispatch({ type: "editTask", payload: { id: task.id , newTask : taskUpdate } })
+    console.log(`dispatch function called inside editTask ${task.id} ${taskUpdate}`)
+    // After calling the reduce function with dispatch, clear out the taskUpdate
+    editToggle()
+    setTaskUpdate('')
+  }
+
   return (
     <>
-   <li>  {task.task} - {task.complete ? 'Completed' : 'Not Completed'}
+      {editButton ? <>
+        <form onSubmit={editTask}>
+          <input type="text" value={taskUpdate} onChange={editedTask => setTaskUpdate(editedTask.target.value)}
+            placeholder="Type here your task..." required />
+          <button type="submit">Save</button>
+          <button onClick={editToggle}>Cancel</button>
+        </form>
+      </> :
+        <li>  {task.task} - {task.complete ? 'Completed' : 'Not Completed'}
 
-    {/* A checkbox next to it which indicates whether it is "complete." */}
-    <label htmlFor="status"> Task Completed </label>
+          {/* A checkbox next to it which indicates whether it is "complete." */}
+          <label htmlFor="status"> Task Completed </label>
 
-    <input type="checkbox" name="status" id="status" 
-    checked={task.complete} // checkbox assiciation with data
-    onChange={()=> {
-    console.log('Checkbox clicked for task:', task.id) // Debugging log
-    {dispatch({type: "completeCheck", payload : {id :task.id}})}
-    console.log(`Dispatch function sent to reducer from TodoItemList component`)
-    }}/>
+          <input type="checkbox" name="status" id="status"
+            checked={task.complete} // checkbox association with data
+            onChange={() => {
+              console.log('Checkbox clicked for task:', task.id) // Debugging log
+              { dispatch({ type: "completeCheck", payload: { id: task.id } }) }
+              console.log(`Dispatch function sent to reducer from TodoItemList component`)
+            }} />
 
-    {/* The "delete" button should be disabled unless the todo is complete! */}
-    {task.complete ? <button id="delete">Delete</button> : null}
+          {/* The "delete" button should be disabled unless the todo is complete! */}
+          {task.complete ? <button id="delete">Delete</button> : null}
 
-    {/* An "edit" button that replaces the todo string with a text input used to edit the todo. */}
-    <button id="edit">Edit</button>
-    {/*Hint: bind the value of this text input to a piece of state so that 
+          {/* An "edit" button that replaces the todo string with a text input used to edit the todo. */}
+          <button onClick={editToggle} id="edit">Edit</button>
+          {/*Hint: bind the value of this text input to a piece of state so that 
     it is always accurate, even when first displayed! */}
 
-    {/* When this text input is active, the "delete" and "edit" buttons should 
+          {/* When this text input is active, the "delete" and "edit" buttons should 
     be hidden, and a "save" button should appear. The "save" button should 
     save any changes made to the todo within the text input. */}
 
-    <button id="save">Save</button>
 
-    
-    </li>
+        </li>
+
+      }
     </>
   )
 }
--- src/state/taskReducer.js
+++ src/state/taskReducer.js
@@ -14,20 +14,33 @@ function reducer(taskList, action) {
             return updatedList
 
         case ("completeCheck"):
-           return taskList.map(task => {
-            console.log(`Check id's: ${task.id} -  ${action.payload.id}`) //Debugger line
+            return taskList.map(task => {
+                console.log(`Check id's completeCheck: ${task.id} -  ${action.payload.id}`) //Debugger line
                 if (task.id === action.payload.id) {
                     console.log(`Task status before check : ${task.complete}`) //Debugger line
                     const updatedTask = { ...task, complete: !task.complete } // return a new task updated
-                    
+
                     console.log(`Updated Task : ${JSON.stringify(updatedTask)}`) //Debugger line
 
                     return updatedTask
                 }
                 console.log("Checkbox not updated") //Debugger line
-                return task 
+                return task
             })
-    
+
+        case ("editTask"):
+           return  taskList.map(task => {
+            console.log(`Check id's edit task: ${task.id} -  ${action.payload.id}`) //Debugger line
+                if (task.id === action.payload.id) {
+                    console.log(`Task text before check : ${task.task}`) //Debugger line
+                    const updatedTask = { ...task, task: action.payload.newTask }
+                    console.log(`Updated Task : ${JSON.stringify(updatedTask)}`) //Debugger line
+                    
+                    return updatedTask
+                }
+                return task
+            })
+
         default:
             return taskList
     }
